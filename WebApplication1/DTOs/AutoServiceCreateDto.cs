acb74c4775133be7a40efab1bb8f32b57ac9ad6d
Вадим <werty193842@gmail.com>
2025-04-26 18:07:00 +0300

    Update 2.2

--- WebApplication1/DTOs/AutoServiceCreateDto.cs
+++ WebApplication1/DTOs/AutoServiceCreateDto.cs
@@ -4,6 +4,7 @@ namespace WebApplication1.DTOs;
 
 public class AutoServiceCreateDto
 {
+    public int AutoServiceId { get; set; }
     [Required] public string Name { get; set; }
     [Required] public string Address { get; set; }
     [Required] public string ContactPhone { get; set; }
